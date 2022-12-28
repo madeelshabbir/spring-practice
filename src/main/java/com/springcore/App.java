45758c81911659d97d32147aadeb0ecf932ee4f5
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-12-28 15:57:03 +0500

    Added property using p tag

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -13,7 +13,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student student = (Student) context.getBean("student1");
-        System.out.println(student);
+        Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student1);
+        System.out.println(student2);
     }
 }
