d7ae903dd1fafcf7507d18a96c3de5242779eaef
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-12-30 18:11:39 +0500

    Constructor injection

--- src/main/java/com/springcore/references/Test.java
+++ src/main/java/com/springcore/references/Test.java
@@ -8,9 +8,15 @@ public class Test {
     ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/references/ref-config.xml");
     A a = (A) context.getBean("aref");
     A a1 = (A) context.getBean("aref1");
+    A ac = (A) context.getBean("arefc");
+    A ac1 = (A) context.getBean("arefc1");
     System.out.println(a.getX());
     System.out.println(a.getOb().getY());
     System.out.println(a1.getX());
     System.out.println(a1.getOb().getY());
+    System.out.println(ac.getX());
+    System.out.println(ac.getOb().getY());
+    System.out.println(ac1.getX());
+    System.out.println(ac1.getOb().getY());
   }
 }
